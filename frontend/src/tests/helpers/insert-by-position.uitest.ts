fceebdcbe02b5b8073ad3341d5cf76a088a7f86c
ilya.lopatin.by <ilya.lopatin@zensoft.io>
2019-09-28 21:54:27 +0300

    setting unit test

--- frontend/src/tests/helpers/insert-by-position.uitest.ts
+++ frontend/src/tests/helpers/insert-by-position.uitest.ts
@@ -1,13 +1,13 @@
-// import {insertByPosition} from "../../helpers/insert-by-position";
-//
-// describe('test pure function insertByPosition', () => {
-//     it('Insert into array by anchor and position ', () => {
-//         const result = insertByPosition([1, 2, 3], 6, 3, 'BEFORE')
-//         expect(result).toEqual([1, 2, 6, 3]);
-//     });
-//
-//     it('Insert into array at the end if there is no anchor and position', () => {
-//         const result = insertByPosition([1, 2, 3], 6)
-//         expect(result).toEqual([1, 2, 3, 6]);
-//     });
-// });
+import {insertByPosition} from "../../helpers/insert-by-position";
+
+describe('test pure function insertByPosition', () => {
+    it('Insert into array by anchor and position ', () => {
+        const result = insertByPosition([1, 2, 3], 6, 3, 'BEFORE')
+        expect(result).toEqual([1, 2, 6, 3]);
+    });
+
+    it('Insert into array at the end if there is no anchor and position', () => {
+        const result = insertByPosition([1, 2, 3], 6)
+        expect(result).toEqual([1, 2, 3, 6]);
+    });
+});
